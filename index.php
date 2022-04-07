972f2c83805db21f37fc55ca5edf517433455321
zminth <102744137+zminth@users.noreply.github.com>
2022-04-07 09:04:44 +0200

    Grundaufbau der Seite

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
+<!DOCTYPE html>
 <html>
     <head>
-        
+        <title></title>
+        <link rel="stylesheet" href="" />
+        <script src=""></script>
     </head>
+
     <body></body>
 </html>
