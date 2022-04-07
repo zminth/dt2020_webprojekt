0b119fbf99e8fdb538bd95d9d43a3704ade13d48
zminth <102744137+zminth@users.noreply.github.com>
2022-04-07 14:05:48 +0200

    vorlage datenbankverbindung

---
+++ scripts/api/vorlage_datenbankverbindung.php
@@ -0,0 +1,36 @@
+<?php
+    error_reporting(0);
+    header('Content-Type: application/json');
+
+    //Serververbindung
+    $hostname="localhost"; $username="root"; $password=""; $database="ticket_db";
+
+    @$dbVerbindung = new mysqli($hostname, $username, $password, $database);
+    if(mysqli_connect_errno() == 0){
+
+
+        $content = $dbVerbindung->query($mSQL);
+        if ($content->num_rows == 0){
+            $data = [
+                'error' => "Keine Einträge vorhanden",
+                'error_code' => "404",
+                'error_message' => "Data not found"
+            ];
+        }else{
+            while ($obj = $content->fetch_object()){
+
+            }
+        }
+
+    }else{
+        $data = [
+            'error' => "Keine Verbindung zur Datenbank",
+            'error_code' => "600",
+            'error_message' => "No Database Connection"
+        ];
+    }
+
+    if($data){
+        echo json_encode($data);
+    }
+?>
