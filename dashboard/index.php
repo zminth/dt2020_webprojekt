56d0422d02ad2f219d65fda580a1d5bab5c7a356
zminth <102744137+zminth@users.noreply.github.com>
2022-04-27 13:48:41 +0200

    Dashbaord added

---
+++ dashboard/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>Vorlage</title>
+        <link rel="stylesheet" href="" />
+        <link rel="stylesheet" href="scripts/main-layout.css" />
+        <script src="scripts/library.js"></script>
+        <!-- <?php
+            if($_SESSION["authentifiziert"]==false){
+                header("Location: http://127.0.0.1:8080/webprojekt/logon/");
+            }
+        ?> -->
+    </head>
+
+    <body>
+        <div id="navigation">
+            <script>document.getElementById("navigation").innerHTML=createMenu();</script>
+        </div>
+        
+        <div id="head">
+            <div id="head-logonUser">
+                <p>Hauke, Dirk</p>
+                <div id="head-logonUser-settings">
+                    <p>Einstellungen</p>
+                    <p>Abmelden</p>
+                </div>
+            </div>
+        </div>
+        <div id="main">Hauptbereich</div>
+        <div id="footer">Fußbereich</div>
+    </body>
+</html>
