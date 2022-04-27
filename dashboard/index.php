e27d209d8b12543c292af86037dca1d2442a6ec6
zminth <102744137+zminth@users.noreply.github.com>
2022-04-27 14:01:19 +0200

    Dashboard bearbeitet

--- dashboard/index.php
+++ dashboard/index.php
@@ -2,13 +2,14 @@
 <html>
     <head>
         <meta charset="utf-8"/>
-        <title>Vorlage</title>
+        <title>Dashboard - Ticketsystem</title>
         <link rel="stylesheet" href="" />
-        <link rel="stylesheet" href="scripts/main-layout.css" />
-        <script src="scripts/library.js"></script>
+        <link rel="stylesheet" href="../scripts/main-layout.css" />
+        <link rel="stylesheet" href="../scripts/dashboard.css" />
+        <script src="../scripts/library.js"></script>
         <!-- <?php
             if($_SESSION["authentifiziert"]==false){
-                header("Location: http://127.0.0.1:8080/webprojekt/logon/");
+                //header("Location: http://127.0.0.1:8080/webprojekt/logon/");
             }
         ?> -->
     </head>
@@ -20,14 +21,17 @@
         
         <div id="head">
             <div id="head-logonUser">
-                <p>Hauke, Dirk</p>
+                <!-- <p>Hauke, Dirk</p>
                 <div id="head-logonUser-settings">
                     <p>Einstellungen</p>
                     <p>Abmelden</p>
-                </div>
+                </div> -->
             </div>
         </div>
-        <div id="main">Hauptbereich</div>
+        <div id="main">
+            <div id="main-diagram-createdTickets"></div>
+            <div id="main-diagram-overviewLast20Days"></div>
+        </div>
         <div id="footer">Fußbereich</div>
     </body>
 </html>
