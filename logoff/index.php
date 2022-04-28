ca0721f3fac8152c080363c02a1420645c1e3169
zminth <102744137+zminth@users.noreply.github.com>
2022-04-28 08:59:52 +0200

    Abmeldung implementiert

---
+++ logoff/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>Logoff</title>
+        <link rel="stylesheet" href="" />
+        <link rel="stylesheet" href="../scripts/main-layout.css" />
+        <link rel="stylesheet" href="../scripts/dashboard.css" />
+        <script src="../scripts/library.js"></script>
+        <!-- <?php
+            session_start();
+            session_destroy();
+            if($_SESSION["authentifiziert"]==false){
+                header("Location: http://127.0.0.1:8080/webprojekt/logon/");
+            }
+        ?> -->
+    </head>
+
+    <body>
+        <div id="navigation">
+            <script>document.getElementById("navigation").innerHTML=createMenu();</script>
+        </div>
+        
+        <div id="head">
+            <div id="head-logonUser">
+                <p>
+                    <?php
+                        echo $_SESSION["username"];
+                    ?>
+                </p>
+                <div id="head-logonUser-settings">
+                    <p>Einstellungen</p>
+                    <p>Abmelden</p>
+                </div>
+            </div>
+        </div>
+        <div id="main">
+            <div id="main-diagram-createdTickets"></div>
+            <div id="main-diagram-overviewLast20Days"></div>
+        </div>
+        <div id="footer">Fußbereich</div>
+    </body>
+</html>
