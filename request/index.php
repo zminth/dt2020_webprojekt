213e04b2f6845dab9fc565a606f8f77b22624faa
zminth <102744137+zminth@users.noreply.github.com>
2022-05-04 10:19:15 +0200

    Seite für die Details einer anfrage angelegt

---
+++ request/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>Requests - Ticketsystem</title>
+        <link rel="stylesheet" href="" />
+        <link rel="stylesheet" href="../scripts/main-layout.css" />
+        <link rel="stylesheet" href="../scripts/requests.css" />
+        <script src="../scripts/library.js"></script>
+        <?php
+            session_start();
+            if($_SESSION["authentifiziert"]==false){
+                header("Location: http://127.0.0.1:8080/webprojekt/logon/");
+            }
+        ?>
+    </head>
+
+    <body>
+        <div id="navigation">
+            <script>document.getElementById("navigation").innerHTML=createMenu();</script>
+        </div>
+        
+        <div id="head">
+            <div id="head-logonUser">
+                <p>
+                    <?php
+                        echo $_SESSION["username"];
+                    ?>
+                </p>
+                <div id="head-logonUser-settings">
+                    <script>document.getElementById("head-logonUser-settings").innerHTML=userMenu();</script>
+                </div>
+            </div>
+        </div>
+        <div id="main">Hauptbereich</div>
+        <div id="footer">Fußbereich</div>
+    </body>
+</html>
