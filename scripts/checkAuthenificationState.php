59e5b796da2d8811864545a9d650a8ecab913144
zminth <102744137+zminth@users.noreply.github.com>
2022-05-05 11:08:57 +0200

    checkAuthenificationState

--- scripts/checkAuthenificationState.php
+++ scripts/checkAuthenificationState.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
     if($_SESSION["authentifiziert"]==false){
-        header("Location: ".$_SERVER["HTTP_HOST"]."/webprojekt/logon/");
+        header("Location: ../logon/");
     }
 ?>
