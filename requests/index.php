c5568039e9bd04ab3fce82b5d9792a7d22bcc892
zminth <102744137+zminth@users.noreply.github.com>
2022-05-18 13:41:00 +0200

    Fehler behoben

--- requests/index.php
+++ requests/index.php
@@ -12,7 +12,7 @@
         ?>
     </head>
 
-    <body>
+    <body style="overflow: hidden;" >
         <div id="navigation">
             <script>document.getElementById("navigation").innerHTML=createMenu();</script>
         </div>
@@ -52,7 +52,78 @@
                     <div class="ticket-erstelldatum">27.04.2022</div>
                     <div class="ticket-abschlussdatum">06.05.2022</div>
                 </div>
+                <script>
+                    const xhttp = new XMLHttpRequest();
+                    xhttp.open("GET", "../scripts/api/getTickets.php", false);
+                    xhttp.send();
 
+                    const tickets = JSON.parse(xhttp.response);
+
+                    var ticketRow, ticketNummer, ticketPrio, ticketBetreff, link, ticketStatus, ticketErsteller, ticketKategorie, ticketErstellDatum, ticketAbschlussDatum, a;
+                    var element = document.getElementById("main-ticketübersicht");
+                    var a = 0;
+
+                    while(tickets[a]){
+                        //Ticketzeile erstellen
+                        ticketRow = document.createElement("div");
+                        ticketRow.setAttribute("class", "ticket-row");
+                        
+                        //Ticketnummer
+                        ticketNummer = document.createElement("div");
+                        ticketNummer.setAttribute("class", "ticket-nummer");
+                        ticketNummer.appendChild(document.createTextNode(tickets[a].TicketID));
+                        ticketRow.appendChild(ticketNummer);
+
+                        //Ticket-Prio hinzufügen
+                        ticketPrio = document.createElement("div");
+                        ticketPrio.setAttribute("class","ticket-prio");
+                        ticketPrio.appendChild(document.createTextNode(tickets[a].PrioID));
+                        ticketRow.appendChild(ticketPrio);
+
+                        //Ticketbetreff einfügen
+                        ticketBetreff = document.createElement("div");
+                        ticketBetreff.setAttribute("class","ticket-betreff");
+                        link = document.createElement("a");
+                        link.setAttribute("href", "../request/?id="+tickets[a].TicketID);
+                        link.appendChild(document.createTextNode(tickets[a].Titel));
+                        ticketBetreff.appendChild(link);
+                        ticketRow.appendChild(ticketBetreff);
+
+                        //Ticketstatus
+                        ticketStatus = document.createElement("div");
+                        ticketStatus.setAttribute("class","ticket-status");
+                        ticketStatus.appendChild(document.createTextNode(tickets[a].StatusID));
+                        ticketRow.appendChild(ticketStatus);
+
+                        //Ticketersteller
+                        ticketErsteller = document.createElement("div");
+                        ticketErsteller.setAttribute("class","ticket-ersteller");
+                        ticketErsteller.appendChild(document.createTextNode(tickets[a].ersteller));
+                        ticketRow.appendChild(ticketErsteller);
+
+                        //Ticketkategorie
+                        ticketKategorie = document.createElement("div");
+                        ticketKategorie.setAttribute("class","ticket-kategorie");
+                        ticketKategorie.appendChild(document.createTextNode(tickets[a].KategorieID));
+                        ticketRow.appendChild(ticketKategorie);
+
+                        //TicketErstellDatum
+                        ticketErstellDatum = document.createElement("div");
+                        ticketErstellDatum.setAttribute("class","ticket-erstelldatum");
+                        ticketErstellDatum.appendChild(document.createTextNode(tickets[a].creationDate));
+                        ticketRow.appendChild(ticketErstellDatum);
+
+                        //TicketAbschlussDatum
+                        ticketAbschlussDatum = document.createElement("div");
+                        ticketAbschlussDatum.setAttribute("class","ticket-abschlussdatum");
+                        ticketAbschlussDatum.appendChild(document.createTextNode("abschluss"));
+                        ticketRow.appendChild(ticketAbschlussDatum);
+
+                        //Ticket-Zeile der Übersicht hinzufügen
+                        element.appendChild(ticketRow);
+                        a++;
+                    }
+                </script>
             </div>
         </div>
         <div id="footer">Fußbereich</div>
